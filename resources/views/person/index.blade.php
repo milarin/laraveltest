952ca661cce93cabf2b241222812658a828d73d8
milan <acmilanpooh@gmail.com>
2021-01-31 12:20:27 +0900

    up

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -13,12 +13,11 @@
     <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
     @foreach ($items as $item)
       <tr>
-        <td>{{ $item -> name }}</td>
-        <td>{{ $item -> mail }}</td>
-        <td>{{ $item -> age }}</td>
+        <td>{{ $item -> getData() }}</td>
       </tr>
     @endforeach
   </table>
+
 @endsection
 
 @section('footer')
